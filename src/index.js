6bdf663a09f81a5b85e19f3058e86c40f7d03d61
DilhanRubera <125571040+DilhanRubera@users.noreply.github.com>
2025-02-12 17:04:36 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// Create a root for rendering the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app using StrictMode
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
